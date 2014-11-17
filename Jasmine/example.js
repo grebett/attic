9cc475014ed7e58a7f2dd919c4767244b40a6da3
grebett <gregory.betton@gmail.com>
2014-11-17 15:05:14 +0100

    update attic

--- Jasmine/example.js
+++ Jasmine/example.js
@@ -1,4 +1,7 @@
 
 exports.multiply = function (n1, n2) {
-  return n1 * n2;
+  if (n1 === "clochette" && n2 === "peter")
+    return "je t'aime";
+  else
+    return "t'es moche et tu pues";
 };
--- Jasmine/spec/example.spec.js
+++ Jasmine/spec/example.spec.js
@@ -3,7 +3,7 @@ var calculator = require("../example");
 
 describe("multiplication", function () {
   it("should multiply 2 and 3", function () {
-    var product = calculator.multiply(3, 3);
-    expect(product).toBe(true);
+    var product = calculator.multiply("clochette", "pierre");
+    expect(product).toBe("je t'aime");
   });
 });
